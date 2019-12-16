1da4d04365aa48e3b15ce6470ad640bc497b4c50
Ubuntu <ubuntu@ip-10-0-0-29.eu-central-1.compute.internal>
2019-12-16 15:07:32 +0000

    add initial image4ioapi functions

--- inc/Api/Image4IOApi.php
+++ inc/Api/Image4IOApi.php
@@ -6,5 +6,49 @@
 namespace Inc\Api;
 
 class Image4IOApi{
-    
+
+    private $apiKey;
+    private $apiSecret;
+
+    private $base_url="https://api.image4.io/";
+    private $version="v0.1";
+
+    public function __construct($_key,$_secret){
+        $this->apiKey=$_key;
+        $this->apiSecret=$_secret;
+    }
+
+    public function connect(){
+
+    }
+
+    public function listFolder($path){
+
+    }
+
+    public function get($name){
+
+    }
+
+    public function createFolder($path,$name){
+
+    }
+
+    public function uploadImage($file,$folder){
+
+    }
+
+    public function delete($name){
+
+    }
+
+    public function deleteFolder($path){
+
+    }
+
+    public function getSubscription(){
+
+    }
+
+    //copy, move
 }
